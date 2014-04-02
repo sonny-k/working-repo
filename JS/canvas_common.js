3fa4c72643b8cc994b5696e7b1e163395ec4a279
sonny-k <sonny_kurniawan@pokelabo.co.jp>
2014-04-02 17:55:48 +0900

    Pendulum simulation
    Drawing lines and erasing the previous ones

--- JS/canvas_common.js
+++ JS/canvas_common.js
@@ -5,6 +5,49 @@ var canvasCommon;
         function canvasCommonClass() {
             console.log("Created Canvas Common Class");
         }
+        canvasCommonClass.prototype.startAnimation = function (type, canvasName) {
+            switch (type) {
+                case 0:
+                    this.rotateLine(canvasName, null, null, null, null, null, null);
+            }
+        };
+
+        canvasCommonClass.prototype.rotateLine = function (canvasName, startPointX, startPointY, axisPointX, axisPointY, endPointX, endPointY) {
+            var c = document.getElementById(canvasName);
+            var ctx = c.getContext("2d");
+
+            if (startPointX === null) {
+                startPointX = 10;
+            }
+            if (startPointY === null) {
+                startPointY = 10;
+            }
+
+            if (endPointX === null) {
+                endPointX = 30;
+            }
+            if (endPointY === null) {
+                endPointY = 30;
+            }
+            if (axisPointX === null) {
+                axisPointX = startPointX;
+            }
+            if (axisPointY === null) {
+                axisPointY = startPointY;
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(startPointX, startPointY);
+            ctx.lineTo(endPointX, endPointY);
+            ctx.stroke();
+
+            ctx.clearRect(0, 0, c.width, c.height);
+
+            ctx.beginPath();
+            ctx.moveTo(50, 50);
+            ctx.lineTo(90, 90);
+            ctx.stroke();
+        };
         return canvasCommonClass;
     })();
     canvasCommon.canvasCommonClass = canvasCommonClass;
--- JS/canvas_common.ts
+++ JS/canvas_common.ts
@@ -5,5 +5,51 @@ module canvasCommon{
 		constructor(){
 			console.log("Created Canvas Common Class");
 		}
+		
+		startAnimation(type:number, canvasName:string){
+			switch(type){
+				case 0:
+				this.rotateLine(canvasName, null,null,null,null,null,null);
+			}
+		}
+
+		rotateLine(canvasName:string, startPointX:number, startPointY:number, axisPointX:number, axisPointY:number, endPointX:number, endPointY:number){
+			var c:any = document.getElementById(canvasName);
+			var ctx:any = c.getContext("2d");
+
+			if(startPointX === null){
+				startPointX = 10;
+			}
+			if(startPointY === null){
+				startPointY = 10;
+			}
+
+			if(endPointX === null){
+				endPointX = 30;
+			}
+			if(endPointY === null){
+				endPointY = 30;
+			}
+			if(axisPointX === null){
+				axisPointX = startPointX;
+			}
+			if(axisPointY === null){
+				axisPointY = startPointY;
+			}
+
+			ctx.beginPath();
+			ctx.moveTo(startPointX,startPointY);
+			ctx.lineTo(endPointX, endPointY);
+			ctx.stroke();
+
+			 ctx.clearRect(0, 0, c.width, c.height);
+
+			ctx.beginPath();
+			ctx.moveTo(50,50);
+			ctx.lineTo(90, 90);
+			ctx.stroke();
+
+
+		}
 	}
 }
