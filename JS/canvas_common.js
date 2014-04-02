67a2609c277a1b532e7cf3c4f49e4f525b481ee4
sonny-k <sonny_kurniawan@pokelabo.co.jp>
2014-04-02 15:56:16 +0900

    Project page
    Gave skeleton structure and committed various third party libraries

---
+++ JS/canvas_common.js
@@ -0,0 +1,12 @@
+/// <reference path="./ThirdParty/jquery.d.ts" />
+var canvasCommon;
+(function (canvasCommon) {
+    var canvasCommonClass = (function () {
+        function canvasCommonClass() {
+            console.log("Created Canvas Common Class");
+        }
+        return canvasCommonClass;
+    })();
+    canvasCommon.canvasCommonClass = canvasCommonClass;
+})(canvasCommon || (canvasCommon = {}));
+//# sourceMappingURL=canvas_common.js.map
---
+++ JS/canvas_common.ts
@@ -0,0 +1,9 @@
+/// <reference path="./ThirdParty/jquery.d.ts" />
+
+module canvasCommon{
+	export class canvasCommonClass{
+		constructor(){
+			console.log("Created Canvas Common Class");
+		}
+	}
+}
